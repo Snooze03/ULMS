3c0460f879bf3828544296109dbf1283876edd88
Snooze <baayjohnzeus@gmail.com>
2022-12-02 20:06:54 +0800

    Updated: EditUser logic now works

--- Forms/Admin/FRM_EditUser.cs
+++ Forms/Admin/FRM_EditUser.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Linq.Expressions;
@@ -17,6 +18,8 @@ namespace ULMS_Forms.Forms.Admin
         private DBAccess dbAccess = new DBAccess();
         private DataTable dtUsers = new DataTable();
 
+        private int id;
+
         public FRM_EditUser()
         {
             InitializeComponent();
@@ -28,12 +31,38 @@ namespace ULMS_Forms.Forms.Admin
 
         private void BTN_Update_Click(object sender, EventArgs e)
         {
+            string newName = TXTBX_LastName.Text + ", " + TXTBX_FirstName.Text;
+            string newUserName = TXTBX_UserName.Text;
+            string newPassWord = TXTBX_PassWord.Text;
+            string newRole;
+            string newPhoneNumber = TXTBX_PhoneNumber.Text;
+            string newEmail = TXTBX_Email.Text;
 
+            if (RB_Admin.Checked) newRole = "Admin";
+            else newRole = "Librarian";
+
+            if (IsValid())
+            {
+                string query = "Update TBL_Users SET Name ='" + newName + "', UserName ='" + newUserName + "', PassWord ='" + newPassWord + "', Role ='" + newRole + "', PhoneNumber ='" + newPhoneNumber + "', Email ='" + newEmail + "' WHERE ID ='" + id + "'";
+
+                SqlCommand updateCommand = new SqlCommand(query);
+
+                updateCommand.Parameters.AddWithValue("@newName", newName);
+                updateCommand.Parameters.AddWithValue("@newUserName", newUserName);
+                updateCommand.Parameters.AddWithValue("@newPassWord", newPassWord);
+                updateCommand.Parameters.AddWithValue("@newRole", newRole);
+                updateCommand.Parameters.AddWithValue("@newPhoneNumber", newPhoneNumber);
+                updateCommand.Parameters.AddWithValue("@newEmail", newEmail);
+
+                int row = dbAccess.executeQuery(updateCommand);
+
+                if (row == 1) MessageBox.Show("User has been updated!");
+            }
+            else MessageBox.Show("Empty fields are not allowed");
         }
 
         private void TXTBX_Id_KeyUp(object sender, KeyEventArgs e)
         {
-            int id;
             if (!int.TryParse(TXTBX_Id.Text, out id) && !string.IsNullOrEmpty(TXTBX_Id.Text)) MessageBox.Show("Please enter an ID");
             else fillTXTBX(id);
         }
@@ -60,6 +89,23 @@ namespace ULMS_Forms.Forms.Admin
             }
 
             dtUsers.Clear();
+        }
+
+        // Checks if textboxes are filled
+        private bool IsValid()  
+        {
+            bool validData = true;
+            foreach (Control control in Controls)
+            {
+                if (control is TextBox)
+                {
+                    TextBox textbox = control as TextBox;
+                    validData &= !string.IsNullOrWhiteSpace(textbox.Text);
+                }
+            }
+
+            if (validData) return true;
+            return false;
         }
     }
 }
